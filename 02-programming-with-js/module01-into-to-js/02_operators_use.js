b3f2b11bb99b4c90c3e71501aa6824c3e422feb2
Yuukihana <avoynova00@gmail.com>
2025-03-16 15:22:28 +0100

    adding exercises

--- 02-programming-with-js/module01-into-to-js/02_operators_use.js
+++ 02-programming-with-js/module01-into-to-js/02_operators_use.js
@@ -1,5 +1,6 @@
-var score = 8;
-console.log("Mid-level skills:" + (score > 0 && score < 10));
+// Task 1 - Using logical && Operator
+ var score = 8;
+ console.log("Mid-level skills:" + (score > 0 && score < 10));
 
+// Task 2 - Using logical || Operator
 var timeRemaining = 0;
-var energy = 10;
