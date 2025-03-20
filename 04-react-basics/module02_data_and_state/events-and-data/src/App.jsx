bd3620a1c7ce9e438a55c23d8d06cf76709da8a4
yuukihana <avoynova00@gmail.com>
2025-03-20 20:54:29 +0100

    Learning about data, events and state

--- 04-react-basics/module02_data_and_state/events-and-data/src/App.jsx
+++ 04-react-basics/module02_data_and_state/events-and-data/src/App.jsx
@@ -1,9 +1,16 @@
 import Btn from './components/Btn';
 import './App.css';
+import ModeToggler from './components/ModeToggler';
+import NumberBtn from './components/NumberBtn';
 
 function App() {
   return (
+    <div>
     <Btn />
+    <ModeToggler />
+    <h2>Lab Task: Add a button and handle a click event</h2>
+    <NumberBtn />
+    </div>
   );
 }
 
