9730a3d239f130aee259819a0bfc8264fdd812ec
yuukihana <avoynova00@gmail.com>
2025-03-26 09:02:51 +0100

    finished section and completed lab about conditional rendering, props and state management

--- 04-react-basics/module03_nav_updating_assets/nav-and-rednering/src/App.jsx
+++ 04-react-basics/module03_nav_updating_assets/nav-and-rednering/src/App.jsx
@@ -2,16 +2,21 @@ import './App.css';
 import Aboutme from './components/Aboutme';
 import Homepage from './components/Homepage';
 import { Route, Routes, Link } from 'react-router-dom';
+import Contact from './components/Contact';
+import { useState } from 'react';
+
 function App() {
   return (
     <div>
       <nav>
         <Link to= "/">Homepage</Link>
-        <Link to= "/about-me">About mee</Link>
+        <Link to= "/about-me">About me</Link>
+        <Link to='/contact'>Contact</Link>
       </nav>
       <Routes>
         <Route path = "/" element = {<Homepage />} />
         <Route path = "/about-me" element = {<Aboutme />} />
+        <Route path = '/contact' element={<Contact/>} />
       </Routes>
     </div>
   );
--- 04-react-basics/module03_nav_updating_assets/nav-and-rednering/src/components/Homepage.jsx
+++ 04-react-basics/module03_nav_updating_assets/nav-and-rednering/src/components/Homepage.jsx
@@ -1,6 +1,23 @@
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
+import { useState } from 'react';
+
 function Homepage(){
+const [loggedIn, setloggedIn] = useState(false); //manages loggedIn state
+
+  const login = () => setloggedIn(true); //functions, used to set loggedIn to true or false
+  const logout = () => setloggedIn(false);
     return(
+        <div>
         <h1>Welcome to my React App</h1>
+            {loggedIn ? (
+                <div>
+                    <LogoutButton logout={logout} />
+                </div>
+            ) : (
+                <LoginButton login={login} />
+            )}
+        </div>
     )
 }
 
