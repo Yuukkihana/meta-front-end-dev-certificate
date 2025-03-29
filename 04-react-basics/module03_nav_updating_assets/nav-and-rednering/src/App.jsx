fab80e7d6d83794ca84f68cdc715acaa113c99a4
yuukihana <avoynova00@gmail.com>
2025-03-29 14:08:28 +0100

    finished module 3

--- 04-react-basics/module03_nav_updating_assets/nav-and-rednering/src/App.jsx
+++ 04-react-basics/module03_nav_updating_assets/nav-and-rednering/src/App.jsx
@@ -4,8 +4,11 @@ import Homepage from './components/Homepage';
 import { Route, Routes, Link } from 'react-router-dom';
 import Contact from './components/Contact';
 import { useState } from 'react';
+import samurai from './assets/samurai.jpg'
+import ReactPlayer from 'react-player'
 
 function App() {
+  const randomImageURL = "https://picsum.photos/400/265"
   return (
     <div>
       <nav>
@@ -13,6 +16,18 @@ function App() {
         <Link to= "/about-me">About me</Link>
         <Link to='/contact'>Contact</Link>
       </nav>
+      <img height = {200}
+          src = {samurai}
+          als ="Samurai Girl"
+        /> 
+      <img src={require('./assets/samurai.jpg')} alt="Samurai Girl" height = {300}/>
+      <img src={randomImageURL} alt="Random Image" />
+      <h1>React Player Example</h1>
+      <ReactPlayer
+        url={'https://www.youtube.com/watch?v=t8OZPJfpcTM'}
+        playing={false}
+        volume={0.5}
+      />
       <Routes>
         <Route path = "/" element = {<Homepage />} />
         <Route path = "/about-me" element = {<Aboutme />} />
